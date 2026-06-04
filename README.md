c6632297d597e12b1f3c9e333f9024a538ae8a9a
Stefan <ertl.stefan2012@gmail.com>
2026-06-04 14:39:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Documentation: https://docs.neoforged.net/docs
+
+Registry for creating new items
+
+Client/Server -> Level.isClientSide() FMLEnvironment.getDist()
+
+
---
+++ src/main/java/com/ggmfrankie/ggmpipes/Items/PipeBlock.java
@@ -0,0 +1,9 @@
+package com.ggmfrankie.ggmpipes.Items;
+
+import net.minecraft.world.level.block.Block;
+
+public class PipeBlock extends Block {
+    public PipeBlock(Properties properties) {
+        super(properties);
+    }
+}
