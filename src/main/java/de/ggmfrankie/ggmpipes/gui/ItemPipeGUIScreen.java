6fea31eb8db602b337d9cda5f8781e758d41c395
Stefan <s.ertl@reinhausen.com>
2026-06-18 15:32:15 +0200

    still still working on gui

--- src/main/java/de/ggmfrankie/ggmpipes/gui/ItemPipeGUIScreen.java
+++ src/main/java/de/ggmfrankie/ggmpipes/gui/ItemPipeGUIScreen.java
@@ -1,6 +1,8 @@
 package de.ggmfrankie.ggmpipes.gui;
 
 import de.ggmfrankie.ggmpipes.ggmPipes;
+import de.ggmfrankie.ggmpipes.gui.widget.ToggleButton;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
@@ -15,7 +17,29 @@ import org.jspecify.annotations.Nullable;
 public class ItemPipeGUIScreen extends ScreenBase<ItemPipeGUIMenu> {
     public static final Identifier BACKGROUND_TEXTURE = Identifier.fromNamespaceAndPath(ggmPipes.MODID, "textures/gui/pipe/pipe_gui.png");
 
+    private ToggleButton toggleInsertButton;
+    private ToggleButton toggleExtractButton;
+
     public ItemPipeGUIScreen(ItemPipeGUIMenu container, Inventory inventory, Component title) {
         super(BACKGROUND_TEXTURE, container, inventory, title, 176, 166);
+    }
+
+    @Override
+    public void extractBackground(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        super.extractBackground(graphics, mouseX, mouseY, a);
+    }
+
+    @Override
+    @NullMarked
+    public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float a) {
+        super.extractRenderState(graphics, mouseX, mouseY, a);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        this.toggleInsertButton = new ToggleButton(leftPos + 100, topPos + 20, 50, 20,
+                menu.blockEntity.
+        );
     }
 }
--- src/main/java/de/ggmfrankie/ggmpipes/gui/widget/ToggleButton.java
+++ src/main/java/de/ggmfrankie/ggmpipes/gui/widget/ToggleButton.java
@@ -1,5 +1,7 @@
 package de.ggmfrankie.ggmpipes.gui.widget;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.components.Button;
@@ -8,15 +10,18 @@ import net.minecraft.client.input.InputWithModifiers;
 import net.minecraft.network.chat.Component;
 import org.jspecify.annotations.NullMarked;
 
+
 public class ToggleButton extends AbstractButton {
 
     private boolean state;
     private final ToggleAction action;
+    private final Font font;
 
-    protected ToggleButton(int x, int y, int width, int height, boolean initial, ToggleAction action) {
+    protected ToggleButton(int x, int y, int width, int height, boolean initial, ToggleAction action, Font font) {
         super(x, y, width, height, Component.empty());
         state = initial;
         this.action = action;
+        this.font = font;
         updateMessage();
     }
 
@@ -24,15 +29,22 @@ public class ToggleButton extends AbstractButton {
     @NullMarked
     protected void extractContents(GuiGraphicsExtractor guiGraphicsExtractor, int mouseX, int mouseY, float partialTicks) {
         this.extractDefaultSprite(guiGraphicsExtractor);
-
+        int textColor = 0xFFFFFF;
+        if (!this.active) textColor = 0xA0A0A0;
+        guiGraphicsExtractor.text(font, this.getMessage(),
+                this.getX() + (this.width - this.font.width(this.getMessage())) / 2,
+                this.getY() + (this.height - 8) / 2,
+                textColor
+        );
     }
 
     @Override
     @NullMarked
     public void onPress(InputWithModifiers input) {
-        action.accept(state, this);
         state = !state;
+        action.accept(state, this);
         updateMessage();
+        this.playDownSound(Minecraft.getInstance().getSoundManager());
     }
 
     @Override
